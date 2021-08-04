1eb15b3f4a2bf3ff6c81cd6ac0210357216e2d3f
chrisTobar1999 <72772248+chrisTobar1999@users.noreply.github.com>
2021-08-04 13:10:33 -0700

    Creation of Server class that will handle server communication

---
+++ Server.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    
+    public static void main(String args[] )
+    {
+        
+        //Socket that will be used for the server to communicate
+        ServerSocket serverSocket = new ServerSocket(1234);
+
+        //Attempts to establish a connection and waits for the client
+        Socket clientSocket = serverSocket.accept();
+
+        DataInputStream in = new DataInputStream(clientSocket.getInputStream() );
+
+        String string = (String) in.readUTF();
+
+        System.out.println("message= " + string);
+
+        serverSocket.close();
+    }
+
+}
