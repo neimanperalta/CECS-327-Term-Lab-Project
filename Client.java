bac5a14124d050755067e615892999c40cfe34c4
chrisTobar1999 <72772248+chrisTobar1999@users.noreply.github.com>
2021-08-04 12:30:22 -0700

    Socket that can be used for communcation?

--- Client.java
+++ Client.java
@@ -12,6 +12,9 @@ public class Client {
         Socket hostSock = new Socket("localhost", 32721);
         DataInputStream in = new DataInputStream(in);
 
+        //Attempt at having one socket do commands, and another socket that gives input from those commands
+        Socket writing = new Socket("localhost",32722);
+
 
         //close socket
         hostSock.close();
